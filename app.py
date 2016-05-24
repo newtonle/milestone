3f42109f9ccd2e44779acd3f051d44acab7f44f5
Newton Le <newtonle@gmail.com>
2016-05-23 23:01:44 -0700

    done!

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from bokeh.plotting import figure
 from bokeh.charts import TimeSeries
 from bokeh.embed import components
+import pandas as pd
 import requests
 import os
+import datetime
 
-api_key = os.environ['API_KEY']
+#api_key = os.environ['API_KEY']
+api_key = 'PzaxdxNpHYtDUFzSuMbk'
 app = Flask(__name__)
 app.jinja_env.autoescape = False
 
@@ -18,9 +21,13 @@ def index():
 def graph():
   # Get form info
   ticker = request.form['ticker'].upper()
-  features = request.form['features']
+  features = request.form.getlist('features')
+  print features
   
-  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s/data.json?api_key=%s' % (ticker, api_key)
+  now = datetime.datetime.now()
+  start_date = (now + datetime.timedelta(-30)).strftime('%Y-%m-%d')
+  end_date = now.strftime('%Y-%m-%d')
+  api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s/data.json?api_key=%s&start_date=%s&end_date=%s' % (ticker, api_key, start_date, end_date)
   session = requests.Session()
   session.mount('https://', requests.adapters.HTTPAdapter(max_retries=3))
   raw_data = session.get(api_url)
@@ -29,14 +36,15 @@ def graph():
   
   df = pd.DataFrame(data=temp_df['dataset_data']['data'],columns=temp_df['dataset_data']['column_names'])
   
+  print df
 
   plot = TimeSeries(df,
               title='Data from Quandle WIKI set for ' + ticker,
               x='Date', y=features, legend=True)
 
   script, div = components(plot)
-  return render_template('graph.html', script=script, div=div)
+  return render_template('graph.html', script=script, div=div, ticker=ticker)
 
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(port=33507, debug=True)
